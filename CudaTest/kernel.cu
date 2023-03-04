51331ebf0e42da66741de6c48f5d16a751ce3403
Reut Benisho <xreutx197@gmail.com>
2023-03-04 15:57:42 +0200

    Adding CPU calculation (SIngle & Multi Threaded)

--- CudaTest/kernel.cu
+++ CudaTest/kernel.cu
@@ -3,6 +3,10 @@
 #include "device_launch_parameters.h"
 
 #include <stdio.h>
+#include <thread>
+#include <vector>
+#include <time.h>
+#include <iostream>
 
 cudaError_t addWithCuda(int *c, const int *a, const int *b, unsigned int size);
 
@@ -12,29 +16,95 @@ __global__ void addKernel(int *c, const int *a, const int *b)
     c[i] = a[i] + b[i];
 }
 
+void addArraysSingleThreadCPU(const int* a, const int* b, int* c, int size, double& time) 
+{
+    clock_t t = clock();
+    for (int i = 0; i < size; ++i)
+        c[i] = a[i] + b[i];
+    t = clock() - t;
+    time = (((double)t) / CLOCKS_PER_SEC) ;  // in seconds
+}
+
+void addArraysMultiThreadCPU(const int* a, const int* b, int* c, int i)
+{
+    c[i] = a[i] + b[i];
+}
+
 int main()
 {
-    const int arraySize = 5;
-    const int a[arraySize] = { 1, 2, 3, 4, 5 };
-    const int b[arraySize] = { 10, 20, 30, 40, 50 };
-    int c[arraySize] = { 0 };
+    const int arraySizeToPrint = 5;
+    
+    int arraySize = 0;
+    std::cout << "Enter array size:\n" << std::endl;
+    std::cin >> arraySize;
+    while (arraySize > 0)
+    {
+        int* a = new int[arraySize];
+        for (int i = 0; i < arraySize; ++i)
+            a[i] = i + 1;
+        int* b = new int[arraySize];
+        for (int i = 0; i < arraySize; ++i)
+            b[i] = (i + 1) * 10;
+        int* cCUDA = new int[arraySize];
+        int* cSingleThreadCPU = new int[arraySize];
+        int* cMultiThreadCPU = new int[arraySize];
+        for (int i = 0; i < arraySize; ++i)
+            cCUDA[i] = cSingleThreadCPU[i] = cMultiThreadCPU[i] = 0;
+        std::vector<std::thread> threads(arraySize);
+        clock_t t;
+        double time_taken = 0;
 
-    // Add vectors in parallel.
-    cudaError_t cudaStatus = addWithCuda(c, a, b, arraySize);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "addWithCuda failed!");
-        return 1;
-    }
+        // Add vectors in parallel.
+        t = clock();
+        cudaError_t cudaStatus = addWithCuda(cCUDA, a, b, arraySize);
+        if (cudaStatus != cudaSuccess) {
+            fprintf(stderr, "addWithCuda failed!\n");
+            //return 1;
+        }
+        t = clock() - t;
+        time_taken = (((double)t) / CLOCKS_PER_SEC) ; // in seconds
 
-    printf("{1,2,3,4,5} + {10,20,30,40,50} = {%d,%d,%d,%d,%d}\n",
-        c[0], c[1], c[2], c[3], c[4]);
+        printf("\nCUDA took %f seconds: ", time_taken);
+        for (int i = 0; i < arraySizeToPrint; ++i)
+            printf("%d ", cCUDA[i]);
 
-    // cudaDeviceReset must be called before exiting in order for profiling and
-    // tracing tools such as Nsight and Visual Profiler to show complete traces.
-    cudaStatus = cudaDeviceReset();
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaDeviceReset failed!");
-        return 1;
+        //Add vectors in CPU Single thread
+        addArraysSingleThreadCPU(a, b, cSingleThreadCPU, arraySize, time_taken);
+
+        printf("\nCPU Single Thread took %f seconds: ", time_taken);
+        for (int i = 0; i < arraySizeToPrint; ++i)
+            printf("%d ", cSingleThreadCPU[i]);
+
+        //Add vectors in CPU Multi thread
+
+        //t = clock();
+        //for (int i = 0; i < arraySize; ++i)
+        //    threads[i] = std::thread(addArraysMultiThreadCPU, a, b, cMultiThreadCPU, i);
+        //for (auto& th : threads) {
+        //    th.join();
+        //}
+        //t = clock() - t;
+        //time_taken = (((double)t) / CLOCKS_PER_SEC) ;  // in seconds
+
+        //printf("\nCPU Multi Thread took %f seconds: ", time_taken);
+        //for (int i = 0; i < arraySizeToPrint; ++i)
+        //    printf("%d ", cMultiThreadCPU[i]);
+
+        // cudaDeviceReset must be called before exiting in order for profiling and
+        // tracing tools such as Nsight and Visual Profiler to show complete traces.
+        cudaStatus = cudaDeviceReset();
+        if (cudaStatus != cudaSuccess) {
+            fprintf(stderr, "cudaDeviceReset failed!\n");
+            //return 1;
+        }
+        delete[] a;
+        delete[] b;
+        delete[] cCUDA;
+        delete[] cSingleThreadCPU;
+        delete[] cMultiThreadCPU;
+
+        std::cout << "\n\nEnter array size:" << std::endl;
+        std::cin >> arraySize;
     }
 
     return 0;
